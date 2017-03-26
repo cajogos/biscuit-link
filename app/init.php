5f60914cc91b5d7a25bddfbfd1c3f2e9aa8ea4b4
Carlos Ferreira <c@rlos.rocks>
2017-03-26 21:28:20 +0100

    Added php info test file to test .php files

--- app/init.php
+++ app/init.php
@@ -6,6 +6,9 @@
  * - You can also switch the development mode ON or OFF here by changing the DEV_MODE global.
  */
 
+// Start session for the current user
+session_start();
+
 // TODO: Move app configuration to a different file!
 
 // Development Mode: Enable (1) or Disable (0)
@@ -17,10 +20,7 @@ if (DEV_MODE)
     error_reporting(E_ALL);
 }
 
-// Start session here
-session_start();
-
-// Add the app_config.php file
+// Add the configuration file
 require $_SERVER['DOCUMENT_ROOT'] . '../../app/config.php';
 
 // Composer autoload
---
+++ public/tests/info.php
@@ -0,0 +1,10 @@
+<?php
+
+
+
+/**
+ * PHP Information file - use it to test your PHP installation.
+ *
+ * DELETE this file once it has successfully been tested!
+ */
+phpinfo();
